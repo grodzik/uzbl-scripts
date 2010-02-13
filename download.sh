7a4113cfb50b21c7c269a27946cd4cba1bb844fe
Pawel Tomak <satherot@gmail.com>
2010-02-13 20:32:23 +0100

    Now files from servers that don't send proper headers are handled

--- download.sh
+++ download.sh
@@ -2,10 +2,10 @@
 # An enhanced version of the download script supplied with uzbl
 
 # Some sites block the default wget --user-agent..
-GET="wget --user-agent=Firefox --content-disposition --load-cookies=${XDG_DATA_HOME:-${HOME}/.local/share/uzbl}/cookies.txt -nv "
+GET="wget --user-agent=Firefox --content-disposition --load-cookies=${XDG_DATA_HOME:-${HOME}/.local/share}/uzbl/cookies.txt -nv "
 
 dest="${HOME}/Downloads/"
-url="$8"
+url="${8}"
 
 http_proxy="$9"
 export http_proxy
@@ -13,5 +13,14 @@ export http_proxy
 test "x$url" = "x" && { echo "you must supply a url! ($url)"; exit 1; }
 
 cd "$dest"
-cmd=`${GET} "${url}" 2>&1| sed 's/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}[[:blank:]]\+[0-9:]\{8\}[[:blank:]]\+URL[^ ]\+ \[[0-9\/]\+\][[:blank:]]\+->[[:blank:]]\+"\([^"]\+\)".*$/\1/'`
-notify-send "Downloaded" "$cmd"
+afile=`${GET} "${url}" 2>&1| sed 's/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}[[:blank:]]\+[0-9:]\{8\}[[:blank:]]\+URL[^ ]\+ \[[0-9\/]\+\][[:blank:]]\+->[[:blank:]]\+"\([^"]\+\)".*$/\1/'`
+bfile="${cmd%%\?*}"
+if [[ "$afile" != "$bfile" ]]
+then
+    if [[ -e "$bfile" ]]
+    then
+        bfile="$bfile".`ls "$bfile"*|wc -l`
+    fi
+    mv "$afile" "$bfile"
+fi
+notify-send "Downloaded" "$bfile"
