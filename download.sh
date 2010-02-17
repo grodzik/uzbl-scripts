859263b304b7d2a1c5382b52123cb5bdba48ddbe
Pawel Tomak <satherot@gmail.com>
2010-02-17 22:41:47 +0100

    Added 'once' action to edit form, load it and forget about it

--- download.sh
+++ download.sh
@@ -5,6 +5,8 @@
 GET="wget --user-agent=Firefox --content-disposition --load-cookies=${XDG_DATA_HOME:-${HOME}/.local/share}/uzbl/cookies.txt -nv "
 
 dest="${HOME}/Downloads/"
+torrents="${HOME}/.torrents/"
+images="${HOME}/OBRAZY/"
 url="${8}"
 
 http_proxy="$9"
@@ -15,6 +17,13 @@ test "x$url" = "x" && { echo "you must supply a url! ($url)"; exit 1; }
 cd "$dest"
 afile=`${GET} "${url}" 2>&1| sed 's/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}[[:blank:]]\+[0-9:]\{8\}[[:blank:]]\+URL[^ ]\+ \[[0-9\/]\+\][[:blank:]]\+->[[:blank:]]\+"\([^"]\+\)".*$/\1/'`
 bfile="${afile%%\?*}"
+if [[ -n `echo ${bfile}| grep -i "\(gif\|jpg\|jpeg\|png\|bmp\)"` ]]
+then
+    bfile="${images}/$bfile"
+elif [[ -n `echo ${bfile}|grep -i "torrent\$"` ]]
+then
+    bfile="${torrents}/$bfile"
+fi
 if [[ "$afile" != "$bfile" ]]
 then
     if [[ -e "$bfile" ]]
--- eFormFiller.sh
+++ eFormFiller.sh
@@ -73,65 +73,83 @@ action=$1
 
 [ -d $keydir ] || mkdir $keydir || exit 1
 
-if [ "$action" != 'edit' -a  "$action" != 'new' -a "$action" != 'load' -a "$action" != 'add' ]
+if [ "$action" != 'edit' -a  "$action" != 'new' -a "$action" != 'load' -a "$action" != 'add' -a "$action" != 'once' ]
 then
-	action=new
-	[[ -e $keydir/$domain ]] && action=load
+    action=new
+    [[ -e $keydir/$domain ]] && action=load
 elif [ "$action" == 'edit' ] && [[ ! -e $keydir/$domain ]]
 then
-	action=new
+    action=new
 fi
 domain=$(echo $url | sed 's/\(http\|https\):\/\/\([^\/]\+\)\/.*/\2/')
 
 if [ "$action" = 'load' ]
 then
-	[[ -e $keydir/$domain ]] || exit 2
-        if [[ `cat $keydir/$domain|grep "!profile"|wc -l` -gt 1 ]]
-        then
-            menu=`cat $keydir/$domain| \
-                  sed -n 's/^!profile=\([^[:blank:]]\+\)/\1/p'`
-            option=`echo -e -n "$menu"| dmenu -nb "${NB}" -nf "${NF}" -sb "${SB}" -sf "${SF}" -p "${PROMPT}"`
-        fi
+    [[ -e $keydir/$domain ]] || exit 2
+    if [[ `cat $keydir/$domain|grep "!profile"|wc -l` -gt 1 ]]
+    then
+        menu=`cat $keydir/$domain| \
+              sed -n 's/^!profile=\([^[:blank:]]\+\)/\1/p'`
+        option=`echo -e -n "$menu"| dmenu -nb "${NB}" -nf "${NF}" -sb "${SB}" -sf "${SF}" -p "${PROMPT}"`
+    fi
 
-        cat $keydir/$domain | \
-            sed -n -e "/^!profile=${option}/,/^!profile=/p" | \
-            sed -n -e 's/\([^(]\+\)([^)]\+):[ ]*\([^[:blank:]]\+\)/js document.getElementsByName("\1")[0].value="\2";/p' | \
-            sed -e 's/@/\\@/p' >> $fifo
+    cat $keydir/$domain | \
+        sed -n -e "/^!profile=${option}/,/^!profile=/p" | \
+        sed -n -e 's/\([^(]\+\)([^)]\+):[ ]*\([^[:blank:]]\+\)/js document.getElementsByName("\1")[0].value="\2";/p' | \
+        sed -e 's/@/\\@/p' >> $fifo
+elif [ "$action" = "once" ]
+then
+    domain=${domain}"_tmp"
+    echo 'js document.documentElement.outerHTML' | \
+        socat - unix-connect:$socket | \
+        tr -d '\n' | \
+        sed 's/>/>\n/g' | \
+        sed -n 's/.*\(<input[^>]\+>\).*/\1/;/type="\(password\|text\)"/Ip' | \
+        sed 's/\(.*\)\(type="[^"]\+"\)\(.*\)\(name="[^"]\+"\)\(.*\)/\1\4\3\2\5/I' | \
+        sed 's/.*name="\([^"]\+\)".*type="\([^"]\+\)".*/\1(\2): /I' >> $keydir/$domain
+    ${editor} $keydir/$domain
+
+    [[ -e $keydir/$domain ]] || exit 2
+
+    cat $keydir/$domain | \
+        sed -n -e 's/\([^(]\+\)([^)]\+):[ ]*\([^[:blank:]]\+\)/js document.getElementsByName("\1")[0].value="\2";/p' | \
+        sed -e 's/@/\\@/p' >> $fifo
+    rm -f $keydir/$domain
 else
-	if [[ "$action" == 'new' || "$action" == 'add' ]]
-	then
-            if [ "$action" == 'new' ]
-            then
-                echo "!profile=NAME_THIS_PROFILE$RANDOM" > $keydir/$domain
-            else
-                echo "!profile=NAME_THIS_PROFILE$RANDOM" >> $keydir/$domain
-            fi
-            #
-            # 2. and 3. line (tr -d and sed) are because, on gmail login for example, 
-            # <input > tag is splited into lines
-            # ex:
-            # <input name="Email"
-            #        type="text"
-            #        value="">
-            # So, tr removes all new lines, and sed inserts new line after each >
-            # Next sed selects only <input> tags and only with type == "text" or == "password"
-            # If type is first and name is second, then another sed will change their order
-            # so the last sed will make output 
-            #       text_from_the_name_attr(text or password): 
-            #
-            #       login(text):
-            #       passwd(password):
-            #
-	    echo 'js document.documentElement.outerHTML' | \
-	        socat - unix-connect:$socket | \
-                    tr -d '\n' | \
-                    sed 's/>/>\n/g' | \
-                    sed -n 's/.*\(<input[^>]\+>\).*/\1/;/type="\(password\|text\)"/Ip' | \
-                    sed 's/\(.*\)\(type="[^"]\+"\)\(.*\)\(name="[^"]\+"\)\(.*\)/\1\4\3\2\5/I' | \
-                    sed 's/.*name="\([^"]\+\)".*type="\([^"]\+\)".*/\1(\2): /I' >> $keydir/$domain
-	fi
-	[[ -e $keydir/$domain ]] || exit 3 #this should never happen, but you never know.
-	$editor $keydir/$domain #TODO: if user aborts save in editor, the file is already overwritten
+    if [[ "$action" == 'new' || "$action" == 'add' ]]
+    then
+        if [ "$action" == 'new' ]
+        then
+            echo "!profile=NAME_THIS_PROFILE$RANDOM" > $keydir/$domain
+        else
+            echo "!profile=NAME_THIS_PROFILE$RANDOM" >> $keydir/$domain
+        fi
+        #
+        # 2. and 3. line (tr -d and sed) are because, on gmail login for example, 
+        # <input > tag is splited into lines
+        # ex:
+        # <input name="Email"
+        #        type="text"
+        #        value="">
+        # So, tr removes all new lines, and sed inserts new line after each >
+        # Next sed selects only <input> tags and only with type == "text" or == "password"
+        # If type is first and name is second, then another sed will change their order
+        # so the last sed will make output 
+        #       text_from_the_name_attr(text or password): 
+        #
+        #       login(text):
+        #       passwd(password):
+        #
+        echo 'js document.documentElement.outerHTML' | \
+            socat - unix-connect:$socket | \
+            tr -d '\n' | \
+            sed 's/>/>\n/g' | \
+            sed -n 's/.*\(<input[^>]\+>\).*/\1/;/type="\(password\|text\)"/Ip' | \
+            sed 's/\(.*\)\(type="[^"]\+"\)\(.*\)\(name="[^"]\+"\)\(.*\)/\1\4\3\2\5/I' | \
+            sed 's/.*name="\([^"]\+\)".*type="\([^"]\+\)".*/\1(\2): /I' >> $keydir/$domain
+    fi
+    [[ -e $keydir/$domain ]] || exit 3 #this should never happen, but you never know.
+    $editor $keydir/$domain #TODO: if user aborts save in editor, the file is already overwritten
 fi
 
 # vim:fileencoding=utf-8:sw=4
