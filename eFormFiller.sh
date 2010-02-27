6467b741e90fc82a768fad8c85a110b2fc4e03e4
Pawel Tomak <satherot@gmail.com>
2010-02-27 07:45:31 +0100

    some quotations

--- eFormFiller.sh
+++ eFormFiller.sh
@@ -70,14 +70,14 @@ if [ "$action" != 'edit' -a  "$action" != 'new' -a "$action" != 'load' -a "$acti
 then
     action="new"
     [ -e $keydir/$domain ] && action="load"
-elif [ "$action" == 'edit' ] && [ ! -e $keydir/$domain ]
+elif [ "$action" == 'edit' ] && [ ! -e "$keydir/$domain" ]
 then
     action="new"
 fi
 
 if [ "$action" = 'load' ]
 then
-    [ -e $keydir/$domain ] || exit 2
+    [ -e "$keydir/$domain" ] || exit 2
     if [ `cat $keydir/$domain|grep "!profile"|wc -l` -gt 1 ]
     then
         menu=`cat $keydir/$domain| \
@@ -143,8 +143,8 @@ else
             sed 's/\(.*\)\(type="[^"]\+"\)\(.*\)\(name="[^"]\+"\)\(.*\)/\1\4\3\2\5/I' | \
             sed 's/.*name="\([^"]\+\)".*type="\([^"]\+\)".*/\1(\2): /I' >> $keydir/$domain
     fi
-    [ -e $keydir/$domain ] || exit 3 #this should never happen, but you never know.
-    $editor $keydir/$domain #TODO: if user aborts save in editor, the file is already overwritten
+    [ -e "$keydir/$domain" ] || exit 3 #this should never happen, but you never know.
+    $editor "$keydir/$domain" #TODO: if user aborts save in editor, the file is already overwritten
 fi
 
 # vim:fileencoding=utf-8:sw=4
