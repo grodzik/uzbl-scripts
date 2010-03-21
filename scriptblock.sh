702b50e10ccc9fd4897f0019aae3972f7e772ab8
Pawel Tomak <satherot@gmail.com>
2010-03-21 14:24:31 +0100

    Added explanations how to use this script

--- scriptblock.sh
+++ scriptblock.sh
@@ -1,4 +1,37 @@
 #!/bin/sh
+# Per site script/plugin blocker
+#
+# To use this script add:
+# @on_event LOAD_COMMIT spawn @scripts_dir/scriptblock.sh
+# in your config file.
+# When called without any parameter script will set per-domain setting or
+# default ones, if there aren't any for current domain
+#
+# By default it sets the disable_scripts variable to 0 (scripts enabled) and
+# disable_plugins to 1 (plugis are disabled). This can be overriten by setting
+# this variables in config file:
+# set disable_plugins = 0
+# set disable_scripts = 1
+#
+# To manage domains there are four actions:
+# unblock_plugins/unblock_scripts - to unblock plugins/scripts for current
+# domain
+# block_plugins/block_scripts - to block plugins/scripts for current domain
+# For example:
+# @cbind sus = spawn @scripts_dir/scriptblock.sh unblock_scripts
+# @cbind sup = spawn @scripts_dir/scriptblock.sh unblock_plugins
+# @cbind sbs = spawn @scripts_dir/scriptblock.sh block_scripts
+# @cbind sbp = spawn @scripts_dir/scriptblock.sh block_plugins
+#
+# This script also sets two variables to be used in the status bar:
+# scripts_status - <span foreground="#ff0000/#00ff00">scripts</span>
+# plugins_status - <span foreground="#ff0000/#00ff00">plugins</span>
+# scripts/plugins text is in red color when disabled, green when enabled
+# To see these variables add \@scripts_status and/or \@plugins_status to the
+# status_format viariable in the config file.
+#
+# Writen by Paul Tomak <paul.tomak@gmail.com>
+#
 keydir=${XDG_DATA_HOME:-$HOME/.local/share}/uzbl/
 
 [ -d "`dirname $keydir`" ] || exit 1
